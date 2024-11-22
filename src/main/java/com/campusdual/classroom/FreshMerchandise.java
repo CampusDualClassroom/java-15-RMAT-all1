2171400f8a2265f3a6fd7da4b55c55c9d870d357
RMAT-all1 <antithelemon@gmail.com>
2024-11-22 09:51:08 +0100

    Ejercicio 15 RESUELTO

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,47 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expirationDate;
+
+    //CONSTRUCTOR Mercancia fresca con sus datos
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf,
+                            int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    //getSpecificData
+    @Override
+    public String getSpecificData() {
+        //Formatear fecha de caducidad
+        String formattedDate = getFormattedDate(expirationDate);
+
+        //Construir String con caduciad y localización
+        StringBuilder specificData = new StringBuilder();
+        specificData.append("Localizacion: ").append(getLocation()).append("\n");
+        specificData.append("Caducidad: ").append(formattedDate);
+
+        return specificData.toString();
+    }
+
+    //Imprime por pantalla "getSpecificData"
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+    }
+
+    //Formatea fecha
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(date);
+    }
+
+    //Getter y Setter
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public  void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
 }
